43d93f941ca73130fe187922fe8bca8ad878b119
nidong <411870880@qq.com>
2019-12-23 13:30:36 +0800

    merge

---
+++ Week 5/id_415/LeetCode_91_415/Test91.java
@@ -0,0 +1,59 @@
+package com.nidong.shop.shoppingMall.biz.suanfa.week5;
+
+/**
+ * 一条包含字母 A-Z 的消息通过以下方式进行了编码：
+ *
+ * 'A' -> 1
+ * 'B' -> 2
+ * ...
+ * 'Z' -> 26
+ * 给定一个只包含数字的非空字符串，请计算解码方法的总数。
+ *
+ * 示例 1:
+ *
+ * 输入: "12"
+ * 输出: 2
+ * 解释: 它可以解码为 "AB"（1 2）或者 "L"（12）。
+ * 示例 2:
+ *
+ * 输入: "226"
+ * 输出: 3
+ * 解释: 它可以解码为 "BZ" (2 26), "VF" (22 6), 或者 "BBF" (2 2 6) 。
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/decode-ways
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class Test91 {
+    public static int numDecodings(String s) {
+        return getAns(s, 0);
+    }
+
+    private static int getAns(String s, int start) {
+        //划分到了最后返回 1
+        if (start == s.length()) {
+            return 1;
+        }
+        //开头是 0,0 不对应任何字母，直接返回 0
+        if (s.charAt(start) == '0') {
+            return 0;
+        }
+        //得到第一种的划分的解码方式
+        int ans1 = getAns(s, start + 1);
+        int ans2 = 0;
+        //判断前两个数字是不是小于等于 26 的
+        if (start < s.length() - 1) {
+            int ten = (s.charAt(start) - '0') * 10;
+            int one = s.charAt(start + 1) - '0';
+            if (ten + one <= 26) {
+                ans2 = getAns(s, start + 2);
+            }
+        }
+        return ans1 + ans2;
+    }
+
+    public static void main(String[] args) {
+        numDecodings("12");;
+    }
+
+}
